64cf20757f00eed13686017294f8386232292982
c1the <humphreygabrielle771@gmail.com>
2024-09-23 15:31:06 -0400

    modified:   app/job-application-guide/page.tsx

--- app/job-application-guide/page.tsx
+++ app/job-application-guide/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client';
 
 import { Box, Container, VStack, Heading, Text, UnorderedList, ListItem, Link as ChakraLink, useColorModeValue } from '@chakra-ui/react';
